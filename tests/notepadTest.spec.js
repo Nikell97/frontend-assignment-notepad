1db73cf6f5e2137315cba666eaa51a9d0de01174
Nikell97 <nikellmattias@gmail.com>
2023-02-28 16:25:14 +0100

    testing BS

--- tests/notepadTest.spec.js
+++ tests/notepadTest.spec.js
@@ -8,3 +8,27 @@ test('has title', async ({ page }) => {
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Notepad/);
   });
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://nikell97.github.io/frontend-assignment-notepad/');
+  });
+
+  let newNoteList = ['a', 'b', 'c'];
+
+  test('write one new note', async ({ page }) => {
+    // create a new todo locator
+    const newTodo = page.getByPlaceholder('What needs to be done?');
+
+    // Create one todo item.
+    await newTodo.fill(newNoteList[0]);
+    await newTodo.press('Enter');
+
+    let noteList = await page.$('.note-list');
+
+    let noteListItem = await noteList.$('li');
+
+    let noteListItemText = await noteListItem.$('>:nth-child(2)');
+
+    // Check that input is empty.
+    await expect(noteListItemText.toHaveText('a'));
+  }); 
